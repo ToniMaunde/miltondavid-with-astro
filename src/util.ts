cfd191f217dd7ebe65ddf0f7db23a0d4d3c542c9
ToniMaunde <maundemilton@gmail.com>
2024-07-30 21:57:37 +0200

    feat: complete home page restructure

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,5 @@
 import { format } from "date-fns";
 
-export function computeReadingTime(articleBody: string) {
-  // the average
-  const wordsPerMinute = 200;
-  const textLength = articleBody.split(" ").length;
-
-  return Math.ceil(textLength / wordsPerMinute);
-}
-
 export function articleDate(createdAt: string, updatedAt: string) {
   const datePattern = "MMM dd yyyy"
   if (createdAt === updatedAt) return format(new Date(createdAt), datePattern);
