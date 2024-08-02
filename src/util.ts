8c9307c636a78d591d10bf6a43e43ca06e3d5855
ToniMaunde <maundemilton@gmail.com>
2024-08-02 23:20:24 +0200

    feat: style table of contents for light color scheme

--- src/util.ts
+++
@@ -1,7 +0,0 @@
-import { format } from "date-fns";
-
-export function articleDate(createdAt: string, updatedAt: string) {
-  const datePattern = "MMM dd yyyy"
-  if (createdAt === updatedAt) return format(new Date(createdAt), datePattern);
-  return `Updated on ${format(new Date(updatedAt), datePattern)}` ;
-}
