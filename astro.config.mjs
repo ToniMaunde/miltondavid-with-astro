d1877d274d2a01d34f831d1ad2c06964e3d48d09
ToniMaunde <maundemilton@gmail.com>
2024-08-15 18:21:04 +0200

    feat:style: add dark color scheme support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwind from "@astrojs/tailwind";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -14,7 +12,7 @@ export default defineConfig({
   },
   integrations: [mdx({
     rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
-  }), tailwind(), icon()],
+  }), tailwind(),],
   redirects: {
     "/blog/[slug]": "/articles/[slug]"
   },
