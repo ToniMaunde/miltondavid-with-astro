9ad188e9f9c31ea1544ade30d46b5780d12baaac
ToniMaunde <maundemilton@gmail.com>
2026-03-08 13:47:35 +0200

    feat: remove twitter links; remove umami related configuration

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,9 @@
-import type { TArticle } from "./content/config.ts";
-
-export type TTwitterMeta = {
-  name:
-    | "twitter:card"
-    | "twitter:site"
-    | "twitter:creator"
-    | "twitter:title"
-    | "twitter:description";
-  content: string;
-};
+import type { TArticle } from "./content/config.ts"
 
 export type TArticleEntry = {
-  id: string;
-  slug: string;
-  body: string;
-  collection: string;
-  data: TArticle;
-};
+  id: string
+  slug: string
+  body: string
+  collection: string
+  data: TArticle
+}
